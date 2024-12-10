622e1c149ecf4ab2b4bea95e65fdee7cb175ffbd
AbdessadekDev <sbaaiabdessadek@gmail.com>
2024-12-10 20:59:13 +0100

    feat(App): integrate translation and update components for multilingual support - Integrated react-i18next for multilingual support across the app. - Replaced static text with translated strings using useTranslation. - Updated the Carousel components to dynamically load content based on the selected language (from localStorage). - Removed the Loading component, replacing it with a static layout structure for improved user experience. - Added key components: Header, AboutMe, Hero, SwitchMode, Break, and Carousel with dynamic data (skills, projects, testimonials).

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,49 @@
-import Loading from "./components/Loading"
+import { useTranslation } from "react-i18next"
+import Header from "./common/Header"
+import AboutMe from "./components/AboutMe"
+import Carousel from "./components/Carousel"
+import Hero from "./components/Hero"
+import SwitchMode from "./components/SwitchMode"
+import skills from "./data/skills"
+import Break from "./components/Break"
+import projects from "./data/projects"
+import testimonials from "./data/testimonials"
 
 const App = () => {
 
+
+  const { t } = useTranslation();
+  const lang = localStorage.getItem('lang');
+
+
   return (
     <>
-      <Loading />
+      <div className="h-max bg-primary-bg-light dark:bg-primary-bg-dark pt-20 overflow-x-hidden">
+        <Header />
+        <Hero />
+        <AboutMe />
+        <Carousel 
+          title={t('general:skills', { lng: lang || 'en'})}
+          direction="left"
+          speed="slow"
+          standaloneItems={skills}
+        />
+        <Break />
+        <Carousel 
+          title={t('header:nav_projects', { lng: lang || 'en'})}
+          direction="left"
+          speed="slow"
+          projects={projects}
+        />
+        <Carousel 
+          title={t('header:nav_testimonials', { lng: lang || 'en'})}
+          direction="right"
+          speed="slow"
+          testimonias={testimonials}
+        />
+        <Break paddingX={0} />
+        <SwitchMode />
+      </div>
     </>
   )
 }
